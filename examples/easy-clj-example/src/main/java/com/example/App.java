8e370e53c887b5312f3dfa308a78b2fe62c8ead9
Ales Huzik <ahuzik@atlassian.com>
2021-01-29 20:28:00 +1100

    example: add code

--- examples/easy-clj-example/src/main/java/com/example/App.java
+++ examples/easy-clj-example/src/main/java/com/example/App.java
@@ -1,4 +1,6 @@
 package com.example;
+import net.aguzik.java.EasyClj;
+import java.util.function.Consumer;
 
 /**
  * Hello world!
@@ -8,6 +10,14 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("What is your first name?");
+        String firstName = (String)EasyClj.call("(read-line)");
+        EasyClj.call("(println s)", "s", "What is your last name?");
+        String lastName = (String)EasyClj.call("(read-line)");
+        Consumer<String> printGreeting = (String msg) -> { System.out.println(msg); };
+        EasyClj.call("(.accept print-greeting (format \"Hello, %s %s!\" first-name last-name))",
+          "first-name", firstName,
+          "last-name", lastName,
+          "print-greeting", printGreeting);
     }
 }
