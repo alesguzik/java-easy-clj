791025dd5e31b8a880abecbbe89bb0c165391905
Ales Huzik <ahuzik@atlassian.com>
2021-01-29 16:28:25 +1100

    mvn archetype:generate -DarchetypeGroupId=org.apache.maven.archetypes -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4

---
+++ examples/easy-clj-example/src/test/java/net/aguzik/AppTest.java
@@ -0,0 +1,20 @@
+package net.aguzik;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
